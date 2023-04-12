35052c1ddaaa55a6d2ec33f4c78b04918432083f
dorukbasaran <dorukbasaran@hotmail.com>
2023-04-12 19:33:25 +0300

    added queue ds

--- src/data_structures/queue.rs
+++ src/data_structures/queue.rs
@@ -1,110 +1,113 @@
-// // // FIFO
-// // pub trait QueueTrait<'a, T> {
-// //     fn new(
-// //         head: &'a Option<&'a mut QueueType<'a, T>>,
-// //         tail: &'a Option<&'a mut QueueType<'a, T>>,
-// //         length: usize,
-// //     ) -> Queue<'a, T>;
-// //     fn peak(&self) -> &Option<&mut QueueType<T>>;
-// //     fn push(&mut self, queue_item: &'a mut Option<&'a mut QueueType<'a, T>>);
-// // }
+use std::rc::Rc;
+use std::cell::RefCell;
 
-// // pub struct QueueType<'a, T: 'a> {
-// //     pub value: T,
-// //     pub next: &'a mut Option<&'a mut QueueType<'a, T>>,
-// // }
+pub struct Queue<T> {
+    length: usize,
+    head: Option<Rc<RefCell<QueueNode<T>>>>,
+    tail: Option<Rc<RefCell<QueueNode<T>>>>,
+}
 
-// // impl<'a, T: 'a> QueueType<'a, T> {
-// //     pub fn new(value: T, next: &'a mut Option<&'a mut QueueType<'a, T>>) -> Self {
-// //         Self { value, next }
-// //     }
-// // }
+impl<T: Clone> Queue<T> {
+    pub fn new() -> Self {
+        Self {
+            length: 0,
+            head: None,
+            tail: None,
+        }
+    }
 
-// // pub struct Queue<'b, T: 'b> {
-// //     pub head: &'b Option<&'b mut QueueType<'b, T>>,
-// //     pub tail: &'b Option<&'b mut QueueType<'b, T>>,
-// //     pub length: usize,
-// // }
+    pub fn enqueue(&mut self, value: T) {
+        let new_node = Rc::new(RefCell::new(QueueNode {
+            value,
+            next: None,
+        }));
 
-// // impl<'a, T: 'a> QueueTrait<'a, T> for Queue<'a, T> {
-// //     fn new(
-// //         head: &'a Option<&'a mut QueueType<'a, T>>,
-// //         tail: &'a Option<&'a mut QueueType<'a, T>>,
-// //         length: usize,
-// //     ) -> Self {
-// //         Self { head, tail, length }
-// //     }
+        match self.tail.take() {
+            Some(tail) => {
+                tail.borrow_mut().next = Some(Rc::clone(&new_node));
+                self.tail = Some(new_node);
+            }
+            None => {
+                self.head = Some(Rc::clone(&new_node));
+                self.tail = Some(new_node);
+            }
+        }
 
-// //     fn peak(&self) -> &Option<&mut QueueType<T>> {
-// //         self.head
-// //     }
+        self.length += 1;
+    }
 
-// //     fn push(&mut self, queue_item: &'a mut Option<&'a mut QueueType<'a, T>>) {
-// //         if self.head.is_none() {
-// //             self.head = queue_item;
-// //         }
-// //         match self.tail {
-// //             Some(tail) => {
-// //                 tail.next = queue_item;
-// //                 self.tail = queue_item;
-// //                 //tail.next = queue_item;
-// //                 // self.tail = queue_item;
-// //             }
-// //             None => todo!(),
-// //         }
-// //     }
-// // }
 
-// // #[cfg(test)]
-// // mod queue_test {
-// //     use super::*;
+    pub fn dequeue(&mut self) -> Option<T> {
+        self.head.take().map(|head| {
+            if let Some(next) = head.borrow_mut().next.take() {
+                self.head = Some(next);
+            } else {
+                self.tail.take();
+            }
+            self.length -= 1;
+            Rc::try_unwrap(head).ok().unwrap().into_inner().value
+        })
+    }
 
-// //     #[test]
-// //     fn peak_test() {
-// //         // let mut new_queue: Queue<i32> = Queue::new();
-// //         // let new_value = &QueueType::new(48);
-// //         // new_queue.head = Some(new_value);
-// //         // assert_eq!(48, new_queue.peak().unwrap().value);
-// //     }
-// // }
+    pub fn peek(&self) -> Option<Rc<T>> {
+        self.head.as_ref().map(|head| Rc::new(head.borrow().value.clone()))
+    }
 
-// pub struct QueueType<'a, T> {
-//     data: T,
-//     next: &'a T,
-// }
+}
 
-// impl<'a, T> QueueType<'a, T> {
-//     pub fn new(data: T, next: &'a T) -> Self {
-//         Self { data, next }
-//     }
-// }
+pub struct QueueNode<T> {
+    value: T,
+    next: Option<Rc<RefCell<QueueNode<T>>>>,
+}
 
-// pub struct Queue<'a, T> {
-//     pub head: &'a Option<QueueType<'a, T>>,
-//     pub tail: &'a Option<QueueType<'a, T>>,
-//     pub length: usize,
-// }
+#[cfg(test)]
+mod tests {
+    use super::Queue;
 
-// impl<'a, T> Queue<'a, T> {
-//     pub fn new() -> Self {
-//         Self {
-//             head: &None,
-//             tail: &None,
-//             length: 0,
-//         }
-//     }
+    #[test]
+    fn test_new_queue() {
+        let queue: Queue<i32> = Queue::new();
+        assert_eq!(queue.length, 0);
+        assert!(queue.head.is_none());
+        assert!(queue.tail.is_none());
+    }
 
-//     pub fn peak(&self) -> &Option<QueueType<'a, T>> {
-//         &self.head
-//     }
+    #[test]
+    fn test_enqueue() {
+        let mut queue = Queue::new();
+        queue.enqueue(1);
+        assert_eq!(queue.length, 1);
+        assert_eq!(*queue.peek().unwrap(), 1);
+        queue.enqueue(2);
+        assert_eq!(queue.length, 2);
+        assert_eq!(*queue.peek().unwrap(), 1);
+    }
 
-//     pub fn add_to_queue(&mut self, queue_item: Option<QueueType<'a, T>>) {
-//         if self.head.is_none() {
-//             self.head = &queue_item;
-//         }
+    #[test]
+    fn test_dequeue() {
+        let mut queue = Queue::new();
+        queue.enqueue(1);
+        queue.enqueue(2);
+        assert_eq!(queue.dequeue(), Some(1));
+        assert_eq!(queue.length, 1);
+        assert_eq!(*queue.peek().unwrap(), 2);
+        assert_eq!(queue.dequeue(), Some(2));
+        assert_eq!(queue.length, 0);
+        assert!(queue.peek().is_none());
+        assert!(queue.dequeue().is_none());
+    }
 
-//         if self.tail.is_none() {
-//             self.tail.unwrap().next =
-//         }
-//     }
-// }
+    #[test]
+    fn test_peek() {
+        let mut queue = Queue::new();
+        assert!(queue.peek().is_none());
+        queue.enqueue(1);
+        assert_eq!(*queue.peek().unwrap(), 1);
+        queue.enqueue(2);
+        assert_eq!(*queue.peek().unwrap(), 1);
+        queue.dequeue();
+        assert_eq!(*queue.peek().unwrap(), 2);
+    }
+}
+
+
